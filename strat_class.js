ffe13b013f8a19733173f218baf3c058dea416b8
JSMTHWCK <jeffrey.smithwick@gmail.com>
2022-09-08 22:07:34 +0000

    updated strat class

--- strat_class.js
+++ strat_class.js
@@ -1,11 +1,13 @@
+const { range } = require("express/lib/request")
 
 
 class Strat{
-    constructor(board){
-        this.board = board
+    constructor(pid){
+        this.pid = pid
     }
-    choose_move(){
+    chooseMove(board){
         //move_obj = {ship_id : [0, 1], movement : [["w","d","w"],["w","d","w"]]}
+        //move_obj = {ship_id:movement, ship_id:movement}
         moveObj = {shipId:[]}
 
         //for each ship you want to move
@@ -20,4 +22,27 @@ class Strat{
         return moveObj
     }
 
+}
+
+class Dumbboat{
+    constructor(pid){
+        this.pid = pid
+    }
+    chooseMove(board){
+        moveobj = {}
+        for(i = 0;i<board.length; i++){
+            for(j = 0; j < board[0].length; j++){
+                if(board[i][j].length != 0){
+                    if(this.pid == 1){
+                        moveobj[board[i][j]] = "down"
+                    }
+                    if(this.pid == 2){
+                        moveobj[board[i][j]] = "up"
+                    }
+                }
+            }
+        }
+        return moveobj
+
+    }
 }
