705f0a1221341e69850586cd6fc94631e95adae1
b3npaii <benjaminmpark23@gmail.com>
2022-11-01 22:26:22 +0000

    change port

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ io.on('connection', (socket) => {
 });
 
 
-http.listen(3000, () => {
-    console.log('Listening on *:3000');
+http.listen(3003, () => {
+    console.log('Listening on *:3003');
 });
 
 
